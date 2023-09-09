a22a762fef0db6a2967cb3eb53eccafd3483408a
ali josheghani <ali.josheghani2005@gmail.com>
2023-09-09 15:57:08 +0330

    product

--- public/assets/js/magic_request.js
+++ public/assets/js/magic_request.js
@@ -2,6 +2,7 @@ function validateForm(form) {
     let formError = false
     let fields = document.forms[form].getElementsByTagName("input")
     let textareas = document.forms[form].getElementsByTagName("textarea")
+    let selects = document.forms[form].getElementsByTagName("select")
 
     let method = document.forms[form].getAttribute("method")
     let url = document.forms[form].getAttribute("url")
@@ -9,10 +10,16 @@ function validateForm(form) {
     let body = {}
     for (i = 0; i < fields.length; i++)
         if (fields[i].type == "file") body[fields[i].id] = document.querySelector("form[name='" + form + "'] #" + fields[i].id).getAttribute("file")
-        else if(fields[i].id == "account_id") body[fields[i].id] = parseInt(document.querySelector("form[name='" + form + "'] #" + fields[i].id).value)
+        else if(fields[i].id == "account_id" || fields[i].id == "price" || fields[i].id == "stock_count") body[fields[i].id] = parseInt(document.querySelector("form[name='" + form + "'] #" + fields[i].id).value)
         else body[fields[i].id] = document.querySelector("form[name='" + form + "'] #" + fields[i].id).value
     for (i = 0; i < textareas.length; i++)
         body[textareas[i].id] = document.querySelector("form[name='" + form + "'] #" + textareas[i].id).value;
+    for (i = 0; i < selects.length; i++)
+        if(selects[i].id == "categories_id" && selects[i].value == "") body[selects[i].id] = null;
+        else body[selects[i].id] = document.querySelector("form[name='" + form + "'] #" + selects[i].id).value;
+
+
+
 
     // console.log(url)
     console.log(method)
@@ -87,7 +94,7 @@ function MagicRequest(method, url, params = {}) {
     return fetch(base_URL + url, requestOptions).then(response => response.text())
         .then(result => {
             result = JSON.parse(result)
-            if(result.status==200){
+            if(result.status==200 || result.status==201){
                 toastr.success("با موفقیت انجام شد");
                 setTimeout(location.reload(), 500)
             }else{
