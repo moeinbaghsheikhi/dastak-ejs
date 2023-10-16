d5d3672527edc857187e60a1b4169c34b0201bf9
moein <moein.baghsheikhi@gmail.com>
2023-10-16 20:57:08 +0330

    fix changing

--- src/controllers/template.controller.js
+++ src/controllers/template.controller.js
@@ -6,7 +6,7 @@ const request = require('../services/request.service')
 module.exports = {
 	async factor1(req, res, next) {
 		let factor = await request.get('factors/'+req.params.code)
-		console.log(factor.data)
+		// console.log(factor.data)
 		if(factor.status == 404) res.render('404')
 		return res.render('template/factor1',{ data: factor.data, page: req.originalUrl.substr(1).toLowerCase() })
 		
